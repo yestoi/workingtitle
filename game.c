fe7d6b6e9081efec014c09bf8f17da938b06feb5
yestoi <yestoi1@gmail.com>
2009-06-09 09:39:55 -0400

    Got it stable

--- game.c
+++ game.c
@@ -10,7 +10,11 @@ void startgame(void)
 	{
 		printf("# "); //prompt (get user info)
 		fgets(cmdRaw, MAX_CHAR-1, stdin);
+        if (cmdRaw[0] == '\n') continue; //Empty, discard
+        g_strchomp(cmdRaw);
+        
 		cmdList = g_strsplit(cmdRaw, " ", -1);
+
         int i, arg;
         for (i=0;cmdList[i] != NULL;++i){
             arg = i;
@@ -22,31 +26,29 @@ void startgame(void)
 
 int ParseCommand(char **cmdArray ,int arg)
 {
+
     enum CMD
     {
-        cd, //Change Directory
+        cd=1, //Change Directory
         ls, //List Directory
         rm, //Remove file/directory
         touch, //create file
         pwd, //Print Working Directory
         clear, //clear screen (require ncurses)
+        _exit,
     } command;
 
     GData *dl;
     g_datalist_init(&dl); 
-    g_datalist_set_data(&dl, "cd", (gpointer) 0);
-    g_datalist_set_data(&dl, "ls", (gpointer) 1);
-    g_datalist_set_data(&dl, "rm", (gpointer) 2);
-    g_datalist_set_data(&dl, "touch", (gpointer) 3);
-    g_datalist_set_data(&dl, "pwd", (gpointer) 4);
-    g_datalist_set_data(&dl, "clear", (gpointer) 5);
+    g_datalist_set_data(&dl, "cd", (gpointer) 1);
+    g_datalist_set_data(&dl, "ls", (gpointer) 2);
+    g_datalist_set_data(&dl, "rm", (gpointer) 3);
+    g_datalist_set_data(&dl, "touch", (gpointer) 4);
+    g_datalist_set_data(&dl, "pwd", (gpointer) 5);
+    g_datalist_set_data(&dl, "clear", (gpointer) 6);
+    g_datalist_set_data(&dl, "exit", (gpointer) 7);
     
-    if (arg < 1) return 0; //Don't bother with no args
-    
-    command = cd; 
-    if (g_datalist_get_data(&dl, cmdArray[0])) {
-        printf("true\n");
-    }
+    command = (int) g_datalist_get_data(&dl, cmdArray[0]);
 
     switch (command)
     {
@@ -74,9 +76,13 @@ int ParseCommand(char **cmdArray ,int arg)
             printf("clear screen\n");
             break;
 
+        case _exit: //gcc thinks I'm calling a function..
+            exit(EXIT_SUCCESS); //cleanup code
+
         default:
+            printf("bash: %s: command not found\n", cmdArray[0]);
             break;
-    }
+    } 
     return 0;
 }
 
